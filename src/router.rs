120c7527896b01c2bd4148a3b1688cf11af7c8ad
leo <leonardo.santangelo@subbyx.com>
2024-07-23 21:14:51 +0200

    fixed home route detection

--- src/router.rs
+++ src/router.rs
@@ -10,7 +10,7 @@ impl HttpRouter for SimpleRouter {
     fn get_handler(self: &Self, target: &impl AsRef<str>) -> Option<fn (request: HttpRequest) -> HttpResponse> {
         match target.as_ref() {
             target if target.starts_with("/echo") => Some(echo),
-            target if target.starts_with("/") => Some(home),
+            target if target == "/" => Some(home),
             _ => None
         }
     }
