5d8a508fb571e6c589d7cec21f49b992b3b9b4dc
leo <leonardo.santangelo@subbyx.com>
2024-07-23 16:35:53 +0200

    added headers to res

--- src/response.rs
+++ src/response.rs
@@ -1,8 +1,9 @@
-use std::{io::Write, net::TcpStream};
+use std::{collections::HashMap, io::Write, net::TcpStream};
 
 pub struct HTTPResponse {
     pub status_code: usize,
     pub status_message: Option<String>,
+    pub headers: HashMap<String, String>
 }
 
 impl HTTPResponse {
@@ -10,6 +11,7 @@ impl HTTPResponse {
         HTTPResponse {
             status_code: 200,
             status_message,
+            headers: HashMap::default()
         }
     }
 }
@@ -20,9 +22,13 @@ pub trait HTTPResponseWriter {
 
 impl HTTPResponseWriter for TcpStream {
     fn write_http_res(self: &mut Self, res: HTTPResponse) -> std::result::Result<usize, std::io::Error> {
-        let status_line = format!("HTTP/1.1 {} {}\r\n", res.status_code, res.status_message.unwrap_or("".to_owned()));
+        let mut bytes = 0;
 
-        let bytes = self.write(&status_line.into_bytes())?;
+        let status_line = format!("HTTP/1.1 {} {}\r\n", res.status_code, res.status_message.unwrap_or("".to_owned()));
+        bytes += self.write(&status_line.into_bytes())?;
+
+        let headers = "\r\n".to_owned();
+        bytes += self.write(&headers.into_bytes())?;
 
         Ok(bytes)
     }
