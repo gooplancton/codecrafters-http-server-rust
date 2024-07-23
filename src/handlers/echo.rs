050a050f49d87f0f2582884a5f8d3a7f478fb0cf
leo <leonardo.santangelo@subbyx.com>
2024-07-23 21:13:37 +0200

    minor fix

--- src/handlers/echo.rs
+++ src/handlers/echo.rs
@@ -1,7 +1,7 @@
 use crate::{request::HttpRequest, response::{HttpResponse, HttpResponseBuilder}};
 
 pub fn echo(req: HttpRequest) -> HttpResponse {
-    let (_, val) = req.target.split_once("/echo").unwrap();
+    let (_, val) = req.target.split_once("/echo/").unwrap();
 
     HttpResponseBuilder::default()
         .status(200, Some("OK"))
